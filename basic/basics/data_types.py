4e3e459a6ba66f23ce70dd76e0daffa4a7ecc1c1
Tridib Sourav <28973825+TridibSourav@users.noreply.github.com>
2026-05-25 12:49:07 +0600

    Add data types examples in data_types.py

---
+++ basic/basics/data_types.py
@@ -0,0 +1,37 @@
+# Integer
+age = 25
+
+# Float
+price = 99.99
+
+# String
+city = "Dhaka"
+
+# Boolean
+is_logged_in = False
+
+# List
+fruits = ["Apple", "Banana", "Mango"]
+
+# Tupled
+coordinates = (23.8103, 90.4125)
+
+# Set
+unique_numbers = {1, 2, 3, 4}
+
+# Dictionary
+student = {
+    "name": "Rahim",
+    "age": 22
+}
+
+# Printing data types
+
+print(type(age))
+print(type(price))
+print(type(city))
+print(type(is_logged_in))
+print(type(fruits))
+print(type(coordinates))
+print(type(unique_numbers))
+print(type(student))
