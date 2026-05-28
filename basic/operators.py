8b37c4c564e504d75f8b382ffa643bd9e32af366
Tridib Sourav <28973825+TridibSourav@users.noreply.github.com>
2026-05-28 14:28:49 +0600

    operators.py
    Create operators.py

---
+++ basic/operators.py
@@ -0,0 +1,27 @@
+a = 10
+b = 3
+
+# Arithmetic operators
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)
+
+# Comparison operators
+
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+
+# Logical operators
+
+x = True
+y = False
+
+print(x and y)
+print(x or y)
+print(not x)
